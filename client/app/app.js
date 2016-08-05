4a962c7f473865a28898bd8eeb9e8bb9c79435e2
Szemann Peter <szemannp@gmail.com>
2016-08-06 01:08:46 +0200

    route service

--- client/app/app.js
+++ client/app/app.js
@@ -2,13 +2,17 @@ var HaxList = angular.module('HaxList', ['ngAnimate', 'ngRoute']);
 
 HaxList.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-    .when('/training.html', {
+    .when('/training', {
       templateUrl: 'training.html',
-      controller: 'mealController'
+      controller: 'MainController'
     })
     .when('/profile', {
       templateUrl: 'profile.html',
-      controller: 'mealController'
+      controller: 'MainController'
+    })
+    .when('/login', {
+      templateUrl: 'index.html',
+      controller: 'MainController'
     }).otherwise({
       redirectTo: '/login'
     });
@@ -19,7 +23,7 @@ HaxList.factory('Config', function() {
     testUrl: 'http://localhost:3000/test',
     loginUrl: 'http://localhost:3000/login/google',
     baseUrl: 'http://localhost:3000/',
-    landingUrl: 'http://localhost:3000/landing.html'
+    landingUrl: 'http://localhost:3000/index'
   };
 });
 
