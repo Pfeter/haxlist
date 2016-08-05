055081fcc5866d4bbda920b21530efdc35409930
Szemann Peter <szemannp@gmail.com>
2016-08-05 22:29:53 +0200

    markup

--- client/app/app.js
+++ client/app/app.js
@@ -12,16 +12,24 @@ HaxList.factory('Config', function() {
 HaxList.factory('HaxService', function(Config, $http) {
   return {
     connectServer: function() {
-      return $http.get(Config.baseUrl);
+      return $http.get(Config.testUrl);
     },
 
     login: function() {
-      return $http.get(Config.loginUrl);
+      return $http.get('/login/google');
     }
   };
 });
 
-HaxList.controller('MainController', function($scope, $http, HaxService) {
+HaxList.controller('MainController', function($scope, $http, $location, HaxService, Config) {
+
+  $scope.logIn = function() {
+    window.location = Config.loginUrl;
+  };
+
+  $scope.logOut = function() {
+    window.location = Config.baseUrl;
+  };
 
   HaxService.connectServer().success(function(data) {
     $scope.hackers = data;
