e6e7a0e4b271a5dad08aa0a3f13c2d5c4f10825d
Szemann Peter <szemannp@gmail.com>
2016-08-05 21:41:13 +0200

    markup upload

--- client/app/app.js
+++ client/app/app.js
@@ -2,22 +2,29 @@ var HaxList = angular.module('HaxList', ['ngAnimate']);
 
 HaxList.factory('Config', function() {
   return {
-    testUrl: 'http://localhost:3000/test'
+    testUrl: 'http://localhost:3000/test',
+    loginUrl: 'http://localhost:3000/login/google',
+    baseUrl: 'http://localhost:3000/',
+    landingUrl: 'http://localhost:3000/haxlist'
   };
 });
 
 HaxList.factory('HaxService', function(Config, $http) {
   return {
-    testServer: function() {
-      return $http.get(Config.testUrl);
+    connectServer: function() {
+      return $http.get(Config.baseUrl);
+    },
+
+    login: function() {
+      return $http.get(Config.loginUrl);
     }
   };
 });
 
 HaxList.controller('MainController', function($scope, $http, HaxService) {
 
-  HaxService.testServer().success(function(data) {
-    $scope.haxlist = data;
-    console.log($scope.haxlist);
+  HaxService.connectServer().success(function(data) {
+    $scope.hackers = data;
+    console.log($scope.hackers);
   });
 });
